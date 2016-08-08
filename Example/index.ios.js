957921541a6af1eeb8d4921a1f165548f6f643e0
Cocoa Hoo <huminwei@vip.qq.com>
2016-08-08 15:21:18 +0800

    update example with searchBox
    update example with searchBox in the navigation, style like jd header

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -8,18 +8,30 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TextInput,
 } from 'react-native';
 import NavigationBar from './react-native-navigationbar'
 
 class Example extends Component {
   render() {
+    const searchBox1 = (
+        <View style={styles.searchBox}>
+          <Image source={require('./images/header/icon_search.png')} style={styles.searchIcon}/>
+          <TextInput
+            keyboardType='web-search'
+            placeholder='搜索京东商品/店铺'
+            style={styles.inputText}/>
+          <Image source={require('./images/header/icon_voice.png')} style={styles.voiceIcon}/>
+        </View>
+    );
     return (
       <View style={styles.container}>
         <NavigationBar
           backName='back'
           title='爱普云'
-          actionArray={[{title: 'close', onPress: () => console.log('close')}]}
+          searchBox={searchBox1}
         />
       </View>
     );
@@ -29,6 +41,40 @@ class Example extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  searchBox: {
+    height: 30,
+    flexDirection: 'row',
+    flex: 1,  // 类似于android中的layout_weight,设置为1即自动拉伸填充
+    borderRadius: 5,  // 设置圆角边
+    backgroundColor: 'white',
+    alignItems: 'center',
+    marginLeft: 8,
+    marginRight: 12
+  },
+  scanIcon: {
+    height: 26.7,
+    width: 26.7,
+    resizeMode: 'stretch'
+  },
+  searchIcon: {
+    marginLeft: 6,
+    marginRight: 6,
+    width: 16.7,
+    height: 16.7,
+    resizeMode: 'stretch'
+  },
+  voiceIcon: {
+    marginLeft: 5,
+    marginRight: 8,
+    width: 15,
+    height: 20,
+    resizeMode: 'stretch'
+  },
+  inputText: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    fontSize: 14
   }
 });
 
